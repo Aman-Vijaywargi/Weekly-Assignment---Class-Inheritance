a2c87b5e86614a977842d013b487d6b46165c2f6
AmanFinisher <64026019+AmanFinisher@users.noreply.github.com>
2021-06-15 11:17:31 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+const React=require("react")
+const ReactDOM=require("react-dom");
+const Circle=require("./components/circle")
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Circle />, document.getElementById("root"));
