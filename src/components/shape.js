e98368f0496e2b249cc4964064c020bb3ac2ee08
AmanFinisher <64026019+AmanFinisher@users.noreply.github.com>
2021-06-15 11:17:00 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,15 @@
 // declare class
+class Shape {
+    constructor(color) {
+        this.color=color;
+    }
+    drawShape() {
+        return color;
+    }
+    calculateArea() {
 
+    }
+}
+
+module.exports = Shape;
 // export class using module.exports
