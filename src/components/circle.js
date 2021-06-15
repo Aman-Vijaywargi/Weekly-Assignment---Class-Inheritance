77e37d8e4d0b23550186f9ed09ffb34728e0a843
AmanFinisher <64026019+AmanFinisher@users.noreply.github.com>
2021-06-15 11:09:38 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,13 @@
 // import using require
+const shape=require("./shape")
+const react=require("react");
 
 // declare class
+class Circle extends shape {
+    calculateArea() {
 
+    }
+}
 
+module.exports = Circle;
 // export class using module.exports
