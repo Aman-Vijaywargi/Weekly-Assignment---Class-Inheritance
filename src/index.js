9a22c09699433fe8c378d58bfbb12997784dd835
AmanFinisher <64026019+AmanFinisher@users.noreply.github.com>
2021-06-19 20:26:32 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 const App = require('./components/App');
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Circle />, document.getElementById("root"));
