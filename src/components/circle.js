b81f7b6c6d508871069342f8ffbf452792f7477b
AmanFinisher <64026019+AmanFinisher@users.noreply.github.com>
2021-06-19 20:15:50 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,27 +1,10 @@
-// import using require
-var Shape = require('./shape.js');
-var React = require('react');
-// declare class
-class Circle extends Shape{
-    constructor(color) {
-        super(color);
-        // this.radius = radius;
-    }
-
-    calculateArea(){
-      let area = 3.14*this.radius*this.radius;
-      console.log(area);
-      return area;
-    }
-}
-// class Circle extends Shape{
-
-//     constructor(){
-//         super(color);
-//     }
-
-//     calculateArea(){
-//     }
-// }
-module.exports.Circle = Circle;
-// export class using module.exports
+const React = require("react");
+const Shape =require("./shape");
+module.exports = class Circle extends Shape {
+  constructor() {
+    super();
+  }
+  calculateArea() {
+    console.log("Area of cirle");
+  }
+};
